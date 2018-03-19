f782fe54918737b2ad081043996496819a51ffb2
esteban <estebanpw@uma.es>
2018-03-19 11:16:13 +0100

    Improved summaring metric, now greedy

--- bin/generate-one-score.sh
+++ bin/generate-one-score.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 CSV=$1
+TH=$2
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
    echo " ==== ERROR ... you called this script inappropriately."
    echo ""
-   echo "   usage:  $0 <index.csv>"
+   echo "   usage:  $0 <index.csv> <threshold>"
    echo ""
    exit -1
 fi
@@ -18,6 +19,8 @@ currgenome=$(tail -n +2 "$CSV" | head -1 | awk -F "," '{print $6}')
 # fill array of chromosomes similarity
 
 array=()
+arraytosort=()
+names=()
 currsum=0
 othergencounter=0
 # for problems with chromo X and Y
@@ -35,32 +38,85 @@ do
 	
 	if [ "$othergenome" != "$currgenome" ]; then
 	
-		#echo "I have curr gen $currgenome and other gen $othergenome but switching. Currsum= $currsum"
-		# Put value in array
-		array[$highest]=$(awk -v a="$currsum" -v b="$othergencounter" 'BEGIN {print a/b}')
-		#echo "array (should: $currsum) has on $highest -> ${array[${highest}]}"
+		# Sort the array with temporal values
+		sorted=($(printf '%s\n' "${arraytosort[@]}"|sort))
+		# accumulate sum until threshold is reached
+		usedValues=1
+		usedValuesNext=2
+		first=$sorted[$usedValues]
+		next=$sorted[$usedValuesNext]
+		finalvalue=$first
+		divisor=1
+		currdiff=$(awk -v a="$first" -v b="$next" -v c="$TH" 'BEGIN {if(b-a > c) print 1; else print 2; }')
+		while [ $currdiff -eq 1 -a $usedValuesNext -lt $highest ];
+		do
+			finalvalue=$(awk -v a="$finalvalue" -v b="$next" 'BEGIN {print a+b}')
+			usedValues=`expr $usedValues + 1`
+			usedValuesNext=`expr $usedValuesNext + 1`
+			first=$sorted[$usedValues]
+			next=$sorted[$usedValuesNext]
+			divisor=$(awk -v a="$divisor" 'BEGIN {print a+0.1}')
+			
+		done
+
+		# Once we are done, finalvalue has to be divided
+		finalvalue=$(awk -v a="$finalvalue" -v b="$divisor" 'BEGIN {print a/b}')
+		
+		# array holds the results
+		#array[$highest]=$(awk -v a="$currsum" -v b="$othergencounter" 'BEGIN {print a/b}')
+		array[$highest]=$finalvalue
+		
 		highest=`expr $highest + 1`
 		currsum=0
+		names+=($currgenome)
 		lastprint=$currgenome
 		currgenome=$othergenome
 		othergencounter=0
+		unset arraytosort
 	else
 		getvalue=$(echo "$i" | awk -F "," '{print $8}')
-		currsum=$(awk -v a="$currsum" -v b="$getvalue" 'BEGIN {print a=a+(1-b); exit}')
+		# Copy value to array 
+		arraytosort[$othergencounter]=$getvalue
+		#currsum=$(awk -v a="$currsum" -v b="$getvalue" 'BEGIN {print a=a+(1-b); exit}')
 		othergencounter=`expr $othergencounter + 1`
 
 	fi
 
 done < "$1.temp"
 
-if [ "$lastprint" == "$currgenome" ]; then
-	array[$highest]=$(awk -v a="$currsum" -v b="$othergencounter" 'BEGIN {print a/b}')
-	#echo "array (should: $currsum) has on $highest -> ${array[${highest}]}"
-	highest=`expr $highest + 1`
-	currsum=0
-	currgenome=$othergenome
-	othergencounter=0
-fi
+# do the last!!!
+#if [ "$lastprint" == "$currgenome" ]; then
+# Sort the array with temporal values
+sorted=($(printf '%s\n' "${arraytosort[@]}"|sort))
+# accumulate sum until threshold is reached
+usedValues=1
+usedValuesNext=2
+first=$sorted[$usedValues]
+next=$sorted[$usedValuesNext]
+finalvalue=$first
+divisor=1
+currdiff=$(awk -v a="$first" -v b="$next" -v c="$TH" 'BEGIN {if(b-a > c) print 1; else print 2; }')
+while [ $currdiff -eq 1 -a $usedValuesNext -lt $highest ];
+do
+	finalvalue=$(awk -v a="$finalvalue" -v b="$next" 'BEGIN {print a+b}')
+	usedValues=`expr $usedValues + 1`
+	usedValuesNext=`expr $usedValuesNext + 1`
+	first=$sorted[$usedValues]
+	next=$sorted[$usedValuesNext]
+	divisor=$(awk -v a="$divisor" 'BEGIN {print a+0.1}')
+	
+done
+
+# Once we are done, finalvalue has to be divided
+finalvalue=$(awk -v a="$finalvalue" -v b="$divisor" 'BEGIN {print a/b}')
+array[$highest]=$finalvalue
+
+highest=`expr $highest + 1`
+currsum=0
+names+=($currgenome)	
+currgenome=$othergenome
+othergencounter=0
+#fi
 
 
 highest=`expr $highest - 1`
@@ -68,8 +124,10 @@ rm $1.temp
 
 tsum=0
 rm $1.csv.inter
+aux=0
 for ((i = 1; i <= highest; i++)); do
-	echo "${array[${i}]}" >> $1.inter
+	echo "${names[${aux}]} ${array[${i}]}" >> $1.inter
+	aux=`expr $aux + 1`
 	#val=${array[${i}]}
 	#tsum=$(awk -v a="$tsum" -v b="$val" '{print a=a+b}')
 done
